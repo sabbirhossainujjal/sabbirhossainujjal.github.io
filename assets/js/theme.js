c635a27c8da8c88cdde89414619deeaf213a10f9
VirusProton <sabbirhossainujjal.buet@gmail.com>
2024-08-20 17:57:13 +0600

    theme fixed

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -72,7 +72,7 @@ let initTheme = (theme) => {
   if (theme == null || theme == 'null') {
     const userPref = window.matchMedia;
     if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
-        theme = 'dark';
+        theme = 'light';
     }
   }
 
